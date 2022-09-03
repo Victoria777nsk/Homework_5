ea6b4534b86fc2786ffbe0822c90a69a07814ea1
Victoria <victoriaerygina@yandex.ru>
2022-09-03 12:08:19 +0700

    Сумма нечетных элементов

---
+++ Задача_36/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void SumOddNumbers()
+{
+    Console.Write("Введите длину массива: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[number];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    Console.WriteLine($"Массив: [{String.Join(", ", array)}]");
+
+    int sumNumbers = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0) sumNumbers += array[i];
+    }
+    Console.WriteLine($"Сумма чисел нечетных позиций = {sumNumbers}"); 
+}
+
+SumOddNumbers();
+
